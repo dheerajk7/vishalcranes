d00aff8914df763804506b9338c5e0d1120a3793
Dheeraj Kushwah <dheeraj77253@gmail.com>
2024-09-29 21:54:18 +0530

    Revert "Development"

--- public/data/testmonials.ts
+++ public/data/testmonials.ts
@@ -9,21 +9,21 @@ const testmonialsData: TestmonialsTypes[] = [
   {
     id: "1",
     title: "Vedika Kantol",
-    imageUrl: "../svg/testmonial.svg",
+    imageUrl: "/svg/testmonial.svg",
     description:
       "All the cranes are fully functional in nature. They can lift heavy machinery.",
   },
   {
     id: "2",
     title: "Rajesh Joshi",
-    imageUrl: "../svg/testmonial2.svg",
+    imageUrl: "/svg/testmonial.svg",
     description:
       "They make sure that each crane is checked before giving it on hire. Will surely recommend others.",
   },
   {
     id: "3",
     title: "Mugdha Sankpal",
-    imageUrl: "../svg/testmonial.svg",
+    imageUrl: "/svg/testmonial.svg",
     description:
       "This company has a wide range of cranes on hire at very competitive price.",
   },
